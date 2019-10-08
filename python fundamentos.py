c25e803ec2448d03ee7743d18393237774fffa04
ilkyest <54871481+ilkyest@users.noreply.github.com>
2019-10-08 05:41:37 -0300

    Add files via upload

---
+++ python fundamentos.py
@@ -0,0 +1,21 @@
+#Para baixar toda uma biblioteca
+import math
+#após, math e apertar "tab", todas as funções de math estarão disponíveis
+
+#Para baixar apenas uma biblioteca/função específica da biblioteca, exemplo, a biblioteca sqrt
+from math import sqrt
+
+#a vantagem é que você não precisa usar math.sqrt e sim apenas o sqrt, exemplo:
+sqrt (9)
+
+#ainda, importar duas bibliotecas de math
+from math import sqrt, cos
+
+#também, pode-se importar todas as funções, mas sem usar "math e a tecla <tab>" (se você sabe, por exemplo, quais as funções de math)
+from math import *
+
+!pip install graphic
+#instala a biblioteca externa graphic
+
+#Ainda, pode-se importar uma biblioteca, mas fazendo com que ela assuma outro identificador
+import math as np
